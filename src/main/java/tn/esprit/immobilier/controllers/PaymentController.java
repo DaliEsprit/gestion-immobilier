f8890699a5f8dbd7f8441df3e940a051d1e14cd6
anaschaarii <anas.chaari@esprit.tn>
2023-06-26 16:02:00 +0100

    update payment service

---
+++ src/main/java/tn/esprit/immobilier/controllers/PaymentController.java
@@ -0,0 +1,29 @@
+package tn.esprit.immobilier.controllers;
+
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.immobilier.entities.Payement;
+import tn.esprit.immobilier.services.IPayementService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+    IPayementService iPayementService;
+    @PostMapping("/add-payment")
+    public Payement ajouterPayement(Payement p){
+        return this.iPayementService.ajouterPayement(p);
+    }
+    @GetMapping("/retrieve-all-payment")
+    public List<Payement> retrieveAllPayement(){
+        return this.iPayementService.retrieveAllPayement();
+    }
+    @DeleteMapping("/delete-payment/{idPayment}")
+    public void deletePayment(@PathVariable("idPayment") long id){
+         this.iPayementService.deletePayement(id);
+    }
+    @PutMapping("/update-payment")
+    public Payement updatePayement(Payement payement){
+        return this.iPayementService.updatePayement(payement);
+    }
+}
